0723877593759ac20afad2ab533b12e8c4324397
AhmedElDoweny <ahmed.ali.eldoweny@gmail.com>
2020-02-14 22:20:24 +0200

    add newsletterEmails and contactsEmails

--- models/settingModel.js
+++ models/settingModel.js
@@ -2,33 +2,51 @@ let mongoose = require("mongoose");
 
 // create schema
 let settingSchema = new mongoose.Schema({
-    _id:{
+    _id: {
         type: Number,
         required: true
     },
-    aboutApp:{
-        type: String,
-        required: true
+    aboutApp: {
+        type: String
     },
-    contactEmail:{
-        type: String,
-        required: true
+    contactEmail: {
+        type: String
     },
-    links:{
+    links: {
         type: [{
-            title:{
-                type:String,
+            title: {
+                type: String,
                 required: true
             },
-            link:{
-                type:String,
+            link: {
+                type: String,
                 required: true
             }
         }],
-        required: true
     },
-
+    contactsEmails: [{
+        type: [{
+            id: {
+                type: Number,
+                required: true
+            },
+            name: String,
+            email: {
+                type: String,
+                required: true
+            },
+            subject: {
+                type: String,
+                required: true
+            },
+            message: {
+                type: String,
+                required: true
+            },
+        }]
+    }],
+    newsLetterEmails: [String]
 })
 
 // mapping
-mongoose.model("setting",settingSchema)
+mongoose.model("setting", settingSchema)

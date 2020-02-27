6db89a2e1730d47c621e4c8384ee23bdd78a55a0
Moaz120 <moaz120560@gmail.com>
2020-02-27 10:47:54 +0200

    add admin

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,5 +1,7 @@
 let mongoose = require("mongoose"),
     autoInc = require("mongoose-auto-increment"),
+    bcrypt = require('bcryptjs'),
+    jwt = require('jsonwebtoken'),
     connection = mongoose.createConnection(`mongodb://localhost:27017/ProConstruct`, { useNewUrlParser: true, useUnifiedTopology: true });
 autoInc.initialize(connection)
 
@@ -35,6 +37,36 @@ let adminSchema = new mongoose.Schema({
 
 })
 adminSchema.plugin(autoInc.plugin, { model: 'admin', field: '_id', startAt: 1, incrementBy: 1 })
+
+adminSchema.pre("save", function(next){
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+})
+
+// verify password
+adminSchema.methods.verifyPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+let secret = 'SECRET#123'
+adminSchema.methods.generateJwt = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            name: this.name,
+            email: this.email,
+            role: this.role,
+        },
+        secret,
+        {
+            expiresIn: "1h"
+        });
+}
 
 // mapping
 mongoose.model("admin", adminSchema)
